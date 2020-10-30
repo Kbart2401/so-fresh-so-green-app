a84f39591d9bf569aa1ced11ca3e9b71a05756fc
Robin Scavo <sirscavo@gmail.com>
2020-10-30 16:20:41 -0600

    setup

---
+++ config/database.js
@@ -0,0 +1,18 @@
+
+
+const {
+  username,
+  password,
+  database,
+  host,
+} = require('./index').db;
+
+module.exports = {
+  development: {
+    username,
+    password,
+    database,
+    host,
+    dialect: 'postgres',
+  },
+};
