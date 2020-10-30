3f8a5efe6638112eeb5a10c40ff004c91f99e1c4
Maximos Salzman <maximossalzman@gmail.com>
2020-10-30 19:14:20 -0400

    Setting Up Express-Session

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const { asyncHandler, } = require("../utils")
+const bcrypt = require("bcryptjs")
+const { User, Tweet} = require("../db/models")
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render("create-user", {title})
 });
+
+router.post("/", asyncHandler( async (req, res) => {
+  const { name, city, email, password, bio} = req.body
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  const user = await User.create({ email, city, name, hashedPassword, bio})
+
+  
+
+}))
 
 module.exports = router;
