cc27df01ffc73db51f186a737eda427dd323d3f4
pongdanny <pongdaniel17@gmail.com>
2020-11-02 13:38:32 -0500

    create user

--- app.js
+++ app.js
@@ -1,52 +1,56 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const session = require("express-session")
-const store = require("connect-pg-simple")
-const { sessionSecret } = require('./config');
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const { sessionSecret } = require("./config");
+const { sequelize } = require("./db/models");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
 const app = express();
 
+const store = new SequelizeStore({
+  db: sequelize,
+});
 // view engine setup
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
 app.use(
   session({
+    store,
     secret: sessionSecret,
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
   })
-)
+);
+store.sync();
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { asyncHandler, } = require("../utils")
-const bcrypt = require("bcryptjs")
-const { User, Tweet} = require("../db/models")
+const { asyncHandler } = require("../utils");
+const bcrypt = require("bcryptjs");
+const { User, Tweet } = require("../db/models");
+const csrf = require("csurf");
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render("create-user", {title})
+
+router.get("/", function (req, res, next) {
+  res.render("create-user", { title });
 });
 
-router.post("/", asyncHandler( async (req, res) => {
-  const { name, city, email, password, bio} = req.body
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const { name, city, email, password, bio } = req.body;
 
-  const hashedPassword = await bcrypt.hash(password, 12);
+    const hashedPassword = await bcrypt.hash(password, 12);
 
-  const user = await User.create({ email, city, name, hashedPassword, bio})
-
-  
-
-}))
+    const user = await User.create({ email, city, name, hashedPassword, bio });
+  })
+);
 
 module.exports = router;
