0392fdf360e55ea94be3116a583ebb46b8833560
Kyle Barthelmes <kbart2401@gmail.com>
2020-11-02 15:32:14 -0500

    Add check user auth validators

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,55 @@
 const express = require("express");
 const router = express.Router();
-const { asyncHandler } = require("../utils");
+const { asyncHandler, handleValidationErrors } = require("../utils");
 const bcrypt = require("bcryptjs");
 const { User, Tweet } = require("../db/models");
 const csrf = require("csurf");
+const cookieParser = require('cookie-parser');
+const { check } = require('express-validator');
+const { db } = require("../config");
 /* GET users listing. */
+router.use(cookieParser());
+const csrfProtection = csrf({ cookie: true });
 
-router.get("/", function (req, res, next) {
-  res.render("create-user", { title });
+const validateForm = [
+  check('name')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for name')
+    .isLength({ max: 100 })
+    .withMessage("Name must not be over 100 characters"),
+  check('city')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for city')
+    .isLength({ max: 25 })
+    .withMessage("City must not be over 25 characters"),
+  check('email')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for email')
+    .isLength({ max: 50 })
+    .withMessage("Email must not be over 50 characters")
+    .isEmail()
+    .withMessage('Input is not a valid email')
+    .custom((value) => {
+      return db.User.findOne({
+        where: {
+          email: value
+        }
+      }).then((user) => {
+        if (user) {
+          return Promise.reject('The provided email address is already in use by another account')
+        }
+      })
+    })
+
+
+]
+
+router.get("/", csrfProtection, function (req, res, next) {
+  res.render("create-user", { title: 'Create User', csrfToken: req.csrfToken() });
 });
 
 router.post(
-  "/",
+  "/", csrfProtection, handleValidationErrors,
   asyncHandler(async (req, res) => {
     const { name, city, email, password, bio } = req.body;
 
--- utils.js
+++ utils.js
@@ -1,3 +1,17 @@
-const asyncHandler = (handler) => (req, res, next) => handler(req,res,next).catch(next)
+const { validationResult } = require('express-validator');
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next)
 
-module.exports = { asyncHandler, }
+const handleValidationErrors = (req, res, next) => {
+  const validationErrors = validationResult(req)
+  const errors = validationErrors.array().map(error => error.msg);
+  if (!validationErrors.isEmpty()) {
+    const err = Error('Bad request');
+    err.errors = errors;
+    err.status = 400;
+    err.title = 'Bad request';
+    return next(err);
+  }
+  next();
+}
+
+module.exports = { asyncHandler, handleValidationErrors }
