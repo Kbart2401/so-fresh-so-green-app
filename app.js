2eea3544311650743a1ebca9ee2c32245075af74
Kyle Barthelmes <kbart2401@gmail.com>
2020-11-03 14:09:03 -0500

    Add post basic functionalities

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const { sessionSecret } = require("./config");
 const { sequelize } = require("./db/models");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const postsRouter = require("./routes/posts");
 
 const app = express();
 
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use('/posts', postsRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
