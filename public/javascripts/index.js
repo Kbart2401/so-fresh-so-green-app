e2f6c8bcf17332dc52005c66d68a5786e739cb70
Kyle B <Kbart2401@gmail.com>
2020-11-03 20:32:12 -0500

    Add nav-links under profile functionality

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,7 +2,16 @@ window.addEventListener('DOMContentLoaded', () => {
 
     document.querySelector('.welcomeDiv')
         .addEventListener('click', (e) => {
-            console.log('HELLO!')
+            const navLinks = document.querySelector('.nav-links')
+            const classes = navLinks.getAttribute('class');
+            if (classes.includes('hidden')) {
+                navLinks.classList.remove('hidden')
+                return;
+            }
+            if(!classes.includes('hidden')) {
+                navLinks.classList.add('hidden')
+                return;
+            }
         })
 
 
