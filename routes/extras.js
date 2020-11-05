a77b386e34fad653d1650cc06f25b8620c4d00df
pongdanny <pongdaniel17@gmail.com>
2020-11-05 12:35:44 -0500

    render user on additional pages

--- routes/extras.js
+++ routes/extras.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const routes = express.Router();
+const { restoreUser } = require("../auth");
 
-routes.get("/locals", (req, res) => {
-  res.render("locals");
+routes.get("/locals", restoreUser, (req, res) => {
+  const user = res.locals.user;
+  res.render("locals", { user });
 });
 
-routes.get("/topics", (req, res) => {
+routes.get("/topics", restoreUser, (req, res) => {
+  const user = res.locals.user;
   res.render("topics");
 });
 
-routes.get("/markets", (req, res) => {
+routes.get("/markets", restoreUser, (req, res) => {
+  const user = res.locals.user;
   res.render("markets");
 });
 
-routes.get("/farmers", (req, res) => {
+routes.get("/farmers", restoreUser, (req, res) => {
+  const user = res.locals.user;
   res.render("farmers");
 });
 module.exports = routes;
