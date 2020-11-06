7348542c7b3427d54f782f8f7f609ea81bcaea5d
Kyle Barthelmes <kbart2401@gmail.com>
2020-11-06 14:11:52 -0500

    working on basic functionality of search bar

--- routes/index.js
+++ routes/index.js
@@ -79,4 +79,11 @@ router.get('/', restoreUser, asyncHandler( async function(req, res, next) {
   res.render('index', { title: 'Farm Feed!!!', user, posts});
 }));
 
+/**********Search Bar*********/
+router.get('/:search(\\w+)', restoreUser, asyncHandler(async (req, res) => {
+  const user = res.locals.user;
+  const searchRes = req.params.searchRes
+  res.redirect('/');
+}))
+
 module.exports = router;
