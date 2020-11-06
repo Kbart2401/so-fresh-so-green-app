4c54a9caa6de235b64b7826f2b8c4a863ebe1437
pongdanny <pongdaniel17@gmail.com>
2020-11-06 15:36:32 -0500

    deleted some navlinks

--- routes/extras.js
+++ routes/extras.js
@@ -2,34 +2,14 @@ const express = require("express");
 const routes = express.Router();
 const { restoreUser } = require("../auth");
 
-routes.get("/locals", restoreUser, (req, res) => {
+routes.get("/events", restoreUser, (req, res) => {
   const user = res.locals.user;
-  res.render("locals", { user });
-});
-
-routes.get("/topics", restoreUser, (req, res) => {
-  const user = res.locals.user;
-  res.render("topics", { user });
-});
-
-routes.get("/markets", restoreUser, (req, res) => {
-  const user = res.locals.user;
-  res.render("markets", { user });
-});
-
-routes.get("/farmers", restoreUser, (req, res) => {
-  const user = res.locals.user;
-  res.render("farmers", { user });
+  res.render("events", { user });
 });
 
 routes.get("/faq", restoreUser, (req, res) => {
   const user = res.locals.user;
   res.render("faq", { user });
-});
-
-routes.get("/about", restoreUser, (req, res) => {
-  const user = res.locals.user;
-  res.render("about", { user });
 });
 
 routes.get("/contact", restoreUser, (req, res) => {
